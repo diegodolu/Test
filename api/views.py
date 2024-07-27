f3db8b7acbfdae11cdf6771d2386e960d19c274f
Diego Bejarano <diego.bejarano@tecsup.edu.pe>
2024-07-27 12:01:36 -0500

    put y delete de vista ProgramaList en curso

--- api/views.py
+++ api/views.py
@@ -293,6 +293,20 @@ class ProgramaList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def put(self, request):
+        programa = models.Programa.objects.get(id=request.data['id'])
+        serializer = serializers.ProgramaSerializer(programa, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request):
+        programa = models.Programa.objects.get(id=request.data['id'])
+        programa.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
 
 class ProgramaWeek(APIView):
     permission_classes = [AllowAny]
